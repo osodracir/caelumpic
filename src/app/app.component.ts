a24e0a3deaddd59a2efd85c2a284c17740d95ed4
angular7923 <angular7923@caelumBSB02-03>
2018-09-10 11:25:57 -0300

    Exercício 1.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// Decorator ("anotação")
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // Notação CSS. Poderia ser #app-root (id) ou .app-root (class)
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Compatível com ES6 (2015)
 export class AppComponent {
-  title = 'caelumpic';
+  title = 'Caelum Pic';
 }
