23cbe1292c3f35a90c77a6293db4ab23526d32ec
angular7923 <angular7923@caelumBSB02-03>
2018-09-10 12:54:38 -0300

    Criando componente de foto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FotoModule // Se não colocar aqui, NÃO VAI!
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Este é o módulo raiz.
 })
 export class AppModule { }
---
+++ src/app/foto/foto.module.ts
@@ -0,0 +1,11 @@
+/* Componentes reutilizáveis são colocados em módulos. */
+
+import { NgModule } from '@angular/core';
+
+import { FotoComponent } from './foto.component';
+
+@NgModule({
+    declarations: [FotoComponent],
+    exports: [FotoComponent]
+})
+export class FotoModule {}
