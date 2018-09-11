dcad28654c213647f38f5ef36dc6aebbc4214f86
angular7923 <angular7923@caelumBSB02-03>
2018-09-11 09:29:43 -0300

    Parametrizando o componente foto.

--- src/app/foto/foto.component.ts
+++ src/app/foto/foto.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component ({
     selector: 'foto',
     templateUrl: './foto.component.html'
 })
-export class FotoComponent {}
+export class FotoComponent {
+    @Input() titulo;
+    @Input() url;
+}
