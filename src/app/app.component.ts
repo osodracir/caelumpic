d57231dd357d4adf1c96f3557984d64d1dc72c71
angular7923 <angular7923@caelumBSB02-03>
2018-09-11 11:51:15 -0300

    Usando HttpClient.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,17 @@ import { HttpClient } from '@angular/common/http';
 // Compatível com ES6 (2015)
 export class AppComponent {
   title :string = 'Caelum Pic';
+  listaFotos;
 
-  constructor(http :HttpClient) {}
+  constructor(http :HttpClient) {
+    const URL = 'http://localhost:3000/v1/fotos';
+    const that = this;
+
+    const stream = http.get(URL);
+    stream.subscribe(function(dados) {
+      that.listaFotos = dados;
+    }, function(erro) {
+      console.log('Erro! ', erro)
+    });
+  }
 }
