68053ef4cfa70eaa2eadcf907c70f59523c00cb8
angular7923 <angular7923@caelumBSB02-03>
2018-09-12 11:58:50 -0300

    Link específico para o Angular.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 const ROTAS_APP: Routes = [
     { path: '', component: ListagemComponent },
     { path: 'cadastro', component: CadastroComponent },
-    { path: '**', redirectTo: '' } // URL desconhecida.
+    { path: '**', redirectTo: '' } // URL desconhecida. Deverá ser o último registro no vetor.
 ];
 
 export const ROTEAMENTO = RouterModule.forRoot(ROTAS_APP);
