2d76ebdbacb7ae5d75468d7577b83a1fbd9ec1e5
angular7923 <angular7923@caelumBSB02-03>
2018-09-13 11:44:29 -0300

    Inserção de novas imagens.

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Foto } from '../foto/foto';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'cadastro',
     templateUrl: './cadastro.component.html'
 })
 export class CadastroComponent {
+    http: HttpClient;
+    roteador: Router;
     foto: Foto = new Foto();
 
+    constructor(http: HttpClient, roteador: Router) {
+        this.http = http;
+        this.roteador = roteador;
+    }
+
     salvar() {
-        console.log(this.foto);
+        const URL = 'http://localhost:3000/v1/fotos';
+        const OPCOES = {
+            headers: new HttpHeaders({
+                "Content-type": "application/json"
+            })
+        };
+        /* Se for só post(), não vai. */
+        this.http.post(URL, this.foto, OPCOES).subscribe(
+            id => {
+                console.log('Cadastro de fotos. ' + id);
+                this.foto = new Foto();
+                // Retorna para a página de listagem.
+                this.roteador.navigate(['']);
+            },
+            erro => console.log(erro)
+        );
     }
 }
